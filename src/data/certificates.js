20b8be747ffb0b177c1e17474a7bfcafd66e0e3e
devmulkalwar <devmulkalwar95@gmail.com>
2024-10-12 16:28:19 +0530

    udemy certificate added

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -1,10 +1,13 @@
 import smackathonImg from "../assets/Images/smackathonCertificate.jpg";
+import udemyImg from "../assets/Images/udemyCertificate.png";
 
 export const certificates = [
     {
         name: "UpSurge 2k23 Smackathon Certificate",
         image: smackathonImg,
-        
     },
-  
+    {
+        name: " The Complete 2024 Web Development Bootcamp",
+        image: udemyImg,
+    },
 ];
